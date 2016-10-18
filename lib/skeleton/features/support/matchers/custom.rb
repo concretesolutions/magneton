5a0cc29e71fb6d4235f4d2c28eeebea436edd5c6
Filipe Mondaini <filipemondaini@gmail.com>
2016-10-17 22:13:45 -0200

    Translating var 'comparacao' to english

--- lib/skeleton/features/support/matchers/custom.rb
+++ lib/skeleton/features/support/matchers/custom.rb
@@ -18,12 +18,12 @@ RSpec::Matchers.define :visual_match do |expected|
     file_atual = File.join(base_path, 'current_images/') + actual + '.png'
     file_baseline = File.join(base_path, 'baseline/') + expected + '.png'
     file_diff = File.join(base_path, 'diffs/') + actual + '_diff_' + expected + '.png'
-    comparacao = IMATCHER.compare(file_atual, file_baseline)
-    @score = comparacao.score
-    if comparacao.match? != true
-      comparacao.difference_image.save(file_diff)
+    comparison = IMATCHER.compare(file_atual, file_baseline)
+    @score = comparison.score
+    if comparison.match? != true
+      comparison.difference_image.save(file_diff)
     end
-    expect(comparacao.match?).to be true
+    expect(comparison.match?).to be true
   end
   failure_message_for_should do |actual|
     "expected that two images are equals, but they are #{@score} different"
