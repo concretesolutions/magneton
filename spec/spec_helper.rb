6903dd829cf4563c4b5f2f9035a9fe04166601e7
Samanta Cicilia <samantacicilia@CSRJL178M.local>
2016-10-25 09:18:33 -0200

    Add codeclimate report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
+require "codeclimate-test-reporter"
 SimpleCov.start
+CodeClimate::TestReporter.start
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
