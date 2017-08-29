14e1d18372b4418bb29950d09af9fa91f417031c
Samanta Cicilia <samanta.cicilia@concrete.com.br>
2017-08-29 19:29:55 -0300

    acertando o hooks

--- lib/skeleton/features/support/hooks.rb
+++ lib/skeleton/features/support/hooks.rb
@@ -2,14 +2,10 @@ require_relative 'helper.rb'
 ## variable which loads the data file according to the environment
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/config/#{ENVIRONMENT_TYPE}.yaml")
 
-<<<<<<< HEAD
 Before do |feature|
-=======
-Before do |_feature|
   ## variable which loads the data file according to the environment
   CONFIG = YAML.load_file(File.dirname(__FILE__) + "/config/#{ENVIRONMENT_TYPE}.yaml")
 
->>>>>>> 25bca70a1892ec69aaad6e496bed3ac32d59778d
   I18n.config.available_locales = :en
 
   ## configure the chosen browser
@@ -19,7 +15,7 @@ Before do |_feature|
 
   ## set default max wait
   Capybara.default_max_wait_time = 60
-  
+
   ## maximize browser
   unless BROWSER.eql?('poltergeist')
     screen_width = Capybara.page.execute_script("return screen.width;")
